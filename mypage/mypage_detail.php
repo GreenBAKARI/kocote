74745f89a8ed7609f4f39eb5146b2020f59ac271
dendenmuthy <dendenmuthy@gmail.com>
2015-12-15 00:15:57 +0900

    競合に挑戦

--- mypage/mypage_detail.php
+++ mypage/mypage_detail.php
@@ -1,6 +1,48 @@
-<?php echo "mypage_detail.php";
-/* * *
- * * *
- * * */
+<html>
+<head><title>mypage_detail.php</title></head>
+<body>
+
+<?php
+phpinfo();
+$link = mysql_connect ( 'c:\xampp\mysql', 'user', 'pass' );
+
+if (! $link) {
+	die ( '接続失敗です。' . mysql_error () );
+}
+
+print('<p>接続に成功しました。</p>');
+
+/* ヘッダ画像 */
+echo "ヘッダ画像<<br>";
+/* アイコン画像 */
+echo "アイコン画像<br>";
+/* 「編集を確認する」-ボタン */
+echo "「編集を確認する」-ボタン<br>";
+/* 名前 */
+echo "名前<br>";
+/* 性別 */
+echo "性別<br>";
+/* 名前の表記-ラジオボタン */
+echo "名前の表記-ラジオボタン<br>";
+/* 学科-プルダウンメニュー */
+echo "学科-プルダウンメニュー<br>";
+/* 興味・関心のある分野-チェックボックス */
+echo "興味・関心のある分野-チェックボックス<br>";
+/* 自己紹介-入力フォーム */
+echo "自己紹介-入力フォーム<br>";
+/* 立ち上げているイベント */
+echo "立ち上げているイベント<br>";
+/* 参加しているイベント */
+echo "参加しているイベント<br>";
+/* お気に入り登録しているイベント */
+echo "お気に入り登録しているイベント<br>";
+
+$close_flag = mysql_close($link);
+
+if ($close_flag){
+    print('<p>切断に成功しました。</p>');
+}
 ?>
 
+</body>
+</html>
