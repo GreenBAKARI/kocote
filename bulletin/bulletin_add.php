747949b815463d1479ed6ec514fbd2ada9f66442
Saito-Akane <usa_kero@k.vodafone.ne.jp>
2015-12-16 21:58:46 +0900

    DBとの接続部分を記述(なんかエラーでてるので後日修正)

--- bulletin/bulletin_add.php
+++ bulletin/bulletin_add.php
@@ -1,21 +1,15 @@
+<!-- 掲示板追加 -->
+<!-- 参考サイト  http://php5.seesaa.net/category/4121238-1.html-->
 <?php
-//掲示板作成
-function bbs_make($bulletin_title, $category){
-  //とりあえず足してみただけ
-  return $bulletin_title + $category;
-}
+//ファイルの読み込み
+require_once("bulletin.php");
 
-//ここで引数指定
-echo "↓bbs_make関数に渡した引数(123と234)をとりあえず足しただけ<br />";
-echo bbs_make(123, 234);
+//データを取得(要変更)
+$pref_cd = $_POST['cd'];
+$pref_name = $_POST['name'];
+
+//クエリの送信(要変更)
+$sql = "INSERT INTO bb VALUES(".$pref_cd.",'".$pref_name."')";
+$result = executeQuery($sql);
+
 ?>
-
-<!-- フロントやけどとりあえず -->
-<form action="bulletin.php" method="get">
-<select name = "category">
- <option>食</option>
- <option>芸術</option>
- <option>スポーツ</option>
-</select>
-<input type="submit">
-</form>
