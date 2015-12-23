9cdbd75c3b09fec1eefb0fa5c4e77e92d9e481af
dendenmuthy <dendenmuthy@gmail.com>
2015-12-23 21:33:26 +0900

    i

--- mypage/mypage_conf.php
+++ mypage/mypage_conf.php
@@ -1,1 +1,22 @@
-<?php echo "mypage_conf.php"?>
+<?php
+ // データがセットされていれば実行
+ if(isset($_GET['comment'])){
+ $comment = $_GET['comment'];
+ echo $comment;
+}
+?>
+
+<!DOCTYPE html>
+<html lang = "ja">
+<head>
+<meta charset = "UFT-8">
+<title>フォームからデータを受け取る</title>
+</head>
+<body>
+<h1>フォームデータの送信</h1>
+<form action = "mypage_conf.php" method = "get">
+<input type = "text" name ="comment"><br/>
+<input type = "submit" value ="送信">
+</form>
+</body>
+</html>
