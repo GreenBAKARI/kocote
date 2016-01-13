42bb430b89dfcff31178ac727493581a4b8674d6
taiki-uenaka <170289b.kut@gmail.com>
2016-01-14 00:25:21 +0900

    レイアウトと処理を変更しました

--- mypage/mypage.php
+++ mypage/mypage.php
@@ -1,6 +1,6 @@
 <!-- ログインしている利用者のIDを取得 -->
 <?php
-    $user_id = 4;
+    $user_id = 1;
 ?>
 
 <!-- 利用者情報の取得 -->
@@ -48,59 +48,78 @@
     for ($i = 0; $i < $length; $i++) {
               $interest_user[$i] = substr($interest[0], $i, 1);
             }
+    //for文が何回目かを表す変数
+      $count_interest = 0;
+    //何個目に格納するかを表す変数
+      $count_result = 1;
     //配列interest_userの値によって対応する文字列を配列interest_resultに格納する
     for ($i = 0; $i < $length; $i++) {
+      $count_interest += 1;
+      if ($interest_user[$i] == 't' && $count_interest == 1) {
+        $interest_result[$count_result] = アニメ;
+        $count_result += 1;
+      }
 
-      if ($interest_user[$i] == 't' && $i = 0) {
-        $interest_result[$i] = アニメ;
+      else if ($interest_user[$i] == 't' && $count_interest == 2) {
+        $interest_result[$count_result] = 映画;
+        $count_result += 1;
       }
-      /*
-      else if ($interest_user[$i] == 't' && $i = 1) {
-        $interest_result[$i] = 映画;
+      else if ($interest_user[$i] == 't' && $count_interest == 3) {
+        $interest_result[$count_result] = 音楽;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 2) {
-        $interest_result[$i] = 音楽;
+      else if ($interest_user[$i] == 't' && $count_interest == 4) {
+        $interest_result[$count_result] = カメラ;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 3) {
-        $interest_result[$i] = カメラ;
+      else if ($interest_user[$i] == 't' && $count_interest == 5) {
+        $interest_result[$count_result] = グルメ;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 4) {
-        $interest_result[$i] = グルメ;
+      else if ($interest_user[$i] == 't' && $count_interest == 6) {
+        $interest_result[$count_result] = ゲーム;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 5) {
-        $interest_result[$i] = ゲーム;
+      else if ($interest_user[$i] == 't' && $count_interest == 7) {
+        $interest_result[$count_result] = スポーツ;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 6) {
-        $interest_result[$i] = スポーツ;
+      else if ($interest_user[$i] == 't' && $count_interest == 8) {
+        $interest_result[$count_result] = 釣り;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 7) {
-        $interest_result[$i] = 釣り;
+      else if ($interest_user[$i] == 't' && $count_interest == 9) {
+        $interest_result[$count_result] = 天体観測;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 8) {
-        $interest_result[$i] = 天体観測;
+      else if ($interest_user[$i] == 't' && $count_interest == 10) {
+        $interest_result[$count_result] = 動物;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 9) {
-        $interest_result[$i] = 動物;
+      else if ($interest_user[$i] == 't' && $count_interest == 11) {
+        $interest_result[$count_result] = 読書;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 10) {
-        $interest_result[$i] = 読書;
+      else if ($interest_user[$i] == 't' && $count_interest == 12) {
+        $interest_result[$count_result] = 乗り物;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 11) {
-        $interest_result[$i] = 乗り物;
+      else if ($interest_user[$i] == 't' && $count_interest == 13) {
+        $interest_result[$count_result] = ファッション;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 12) {
-        $interest_result[$i] = ファッション;
+      else if ($interest_user[$i] == 't' && $count_interest == 14) {
+        $interest_result[$count_result] = 漫画;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 13) {
-        $interest_result[$i] = 漫画;
+      else if ($interest_user[$i] == 't' && $count_interest == 15) {
+        $interest_result[$count_result] = 料理;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 14) {
-        $interest_result[$i] = 料理;
+      else if ($interest_user[$i] == 't' && $count_interest == 16) {
+        $interest_result[$count_result] = 旅行;
+        $count_result += 1;
       }
-      else if ($interest_user[$i] == t && $i = 15) {
-        $interest_result[$i] = 旅行;
-      }
-      */
     }
       mysql_close($link);
 ?>
@@ -254,7 +273,8 @@
 <title>高知県大学生用交流サイト「KoCo + Te」</title>
 </head>
 <center>
-<link rel="stylesheet" href="../style.css"　type="text/css">
+<link rel="stylesheet" href="../css/style.css"　type="text/css">
+<link rel="stylesheet" href="../css/my_style.css"　type="text/css">
 <body topmargin="100" bottommargin="100">
 
 
@@ -262,123 +282,171 @@
 
 
 <form id="loginForm" name="loginForm" action="" method="POST">
-  <!-- <?php echo $errorMessage ?> -->
+<!-- <?php echo $errorMessage ?> -->
 
-<div id = "box">
-  <a href="http://localhost/kocote/event/event.php"><img src="../img/ev_home.jpg" height="7%" width="16%"></a>
-  <a href="http://localhost/kocote/bulletin/bulletin.php"><img src="../img/bb_home.jpg" height="7%" width="16%"></a>
-  <a href="http://localhost/kocote/search/search.php"><img src="../img/se_home.jpg" height="7%" width="16%"></a>
-  <a href="http://localhost/kocote/dm/dm.php"><img src="../img/dm_home.jpg" height="7%" width="16%"></a>
-  <a href="http://localhost/kocote/mypage/mypage.php"><img src="../img/mp_home.jpg" height="7%" width="16%"></a></div>
-<br><br><br>
+<!-- 機能選択ボタン -->
+<div id="box">
+  <a href="../event/event.php"><img src="../img/ev_home.jpg" height="13%" width="16%"></a>
+  <a href="../bulletin/bulletin.php"><img src="../img/bb_home.jpg" height="13%" width="16%"></a>
+  <a href="../search/search.php"><img src="../img/se_home.jpg" height="13%" width="16%"></a>
+  <a href="../dm/dm.php"><img src="../img/dm_home.jpg" height="13%" width="16%"></a>
+  <a href="../mypage/mypage.php"><img src="../img/mp_home.jpg" height="13%" width="16%"></a></div>
+<br>
 
-
+<!-- ヘッダ画像の表示-->
+<img src="mypage_image.php?id=<?php echo $user_id ?>&image=2" class="header-img">
+<br>
 <!-- アイコン画像の表示-->
-<img src="mypage_image.php?id=<?php echo $user_id ?>&image=1">
-<img src="mypage_image.php?id=<?php echo $user_id ?>&image=2">
+<img src="mypage_image.php?id=<?php echo $user_id ?>&image=1" class="icon-img" style="position:absolute;left:240px;top:450px;">
+
 
 <!-- 編集ボタン-->
 <form>
-<button type="button" name="aaa" value="aaa" onclick="location.href='http://localhost/kocote/mypage/mypage_detail.php?user_id=<?php echo $user_id ?>'">
-<font size="5" color="#333399">編集</font>
+<button class="btn" type="button" name="aaa" value="aaa" onclick="location.href='./mypage_detail.php?user_id=<?php echo $user_id ?>'" style="position:absolute;left:278px;top:640px;">
+<font size="5" >編集する</font>
 </button>
 </form>
 
 <!-- 利用者情報の表示-->
-<p>
-<?php echo $last_name[0], $first_name[0],'　', $sex[0];?> <br />
-<?php echo $college_name[0], '　', $grade[0], '年', '　', $department_name[0];?>
-</p>
+<table  class="mypage-table" style="position:absolute;left:500px;top:450px;">
+  <!-- 利用者名の表示-->
+  <tr>
+    <td class="name-size"><?php echo  $last_name[0], $first_name[0],'　', $sex[0];?></td>
+  </tr>
+  <br>
+  <!-- 利用者の大学、学部、学年を表示-->
+  <tr>
+    <td class="space"><?php echo $college_name[0], '　', $grade[0], '年', '　', $department_name[0];?></td>
+  </tr>
 
-<p> 興味関心のある分野 <br />
-<?php
-    for($i = 0; $i < $length; $i++) {
-      echo $interest_result[$i], '<br />';
-    }
-  ?>
-</p>
+  <!-- 利用者の興味関心のある分野を表示-->
+  <tr>
+    <td class="name-size">興味関心のある分野</td>
+  </tr>
+  <br>
+  <tr>
+    <td class="space">
+      <?php
+          for($i = 1; $i < $count_result; $i++) {
+            //三つ要素を表示すると改行する
+            if( ($i % 3) == 0 ){
+              echo '・', $interest_result[$i], '　<br>';
+            }else {
+              echo '・', $interest_result[$i], '　';
+            }
+          }
+      ?>
+    </td>
+  </tr>
+  <br>
+  <!-- 利用者の自己紹介文を表示-->
+  <tr>
+    <td class="name-size">自己紹介文</td>
+  </tr>
+  <tr>
+    <td class="space"><?php echo $profile[0];?></td>
+  </tr>
 
-<p> 自己紹介文 <br />
-  <?php echo $profile[0];?>
-</p>
+  <!-- 利用者が立ち上げているイベントの表示-->
+  <tr>
+    <td class="name-size">
+      <?php echo $last_name[0], 'さんが立ち上げているイベント', '<br />';?>
+    </td>
+  </tr>
+  <tr>
+    <td class="space">
+      <?php
+          for($i = 0; $i < count($event_id); $i++) {
+            //月の表示（先頭の0は表示されない）
+            if (substr($event_start[$i], 5, 1) == 0) {
+            echo '&nbsp;&thinsp;',substr($event_start[$i], 6, 1).'月';
+            } else {
+            echo substr($event_start[$i], 5, 2).'月';
+            }
+            //日の表示（先頭の0は表示されない）
+            if (substr($event_start[$i], 8, 1) == 0) {
+            echo '&ensp;&thinsp;', substr($event_start[$i], 9, 1).'日 ';
+            } else {
+            echo substr($event_start[$i], 8, 2).'日 ';
+            }
+            //イベント名とイベントの参加人数の表示
+            echo '　', '<a href=../event/event_detail.php?event_id='.$event_id[$i].'>'.$event_title[$i].'</a>',
+            '(', '現在の参加人数:', $EVCNT[$i], '人)',
+            //「編集・削除」ボタンの表示
+            '<form>
+            <button class="btn" type="button" name="aaa" value="aaa" onclick="location.href=\'../event/event_edit.php?event_id='.$event_id[$i].'\'" style="position: relative; left: 370px; top: -30px;">
+            <font size="2">編集・削除</font>
+            </button>
+            </form>';
+          }
+      ?>
+    </td>
+  </tr>
 
-<!-- 利用者が立ち上げているイベントの表示-->
-<p>
-<?php
-    echo $last_name[0], 'さんが参加しているイベント', '<br />';
-    for($i = 0; $i < count($event_id); $i++) {
-      //月の表示（先頭の0は表示されない）
-      if (substr($event_start[$i], 5, 1) == 0) {
-      echo substr($event_start[$i], 6, 1).'月';
-      } else {
-      echo substr($event_start[$i], 5, 2).'月';
-      }
-      //日の表示（先頭の0は表示されない）
-      if (substr($event_start[$i], 8, 1) == 0) {
-      echo substr($event_start[$i], 9, 1).'日 ';
-      } else {
-      echo substr($event_start[$i], 8, 2).'日 ';
-      }
-      //イベント名とイベントの参加人数の表示
-      echo '　', '<a href=http://localhost/kocote/event/event_detail.php?event_id='.$event_id[$i].'>'.$event_title[$i].'</a>',
-      '(', '現在の参加人数:', $EVCNT[$i], '人)', '<br />',
-      //「編集・削除」ボタンの表示
-      '<form>
-      <button type="button" name="aaa" value="aaa" onclick="location.href=\'http://localhost/kocote/mypage/event_edit.php?event_id='.$event_id[$i].'\'">
-      <font size="2" color="#333399">編集・削除</font>
-      </button>
-      </form>';
-    }
-?>
-</p>
+  <!-- 利用者が参加しているイベントの表示-->
+  <tr>
+    <td class="name-size">
+      <?php echo $last_name[0], 'さんが参加しているイベント', '<br />'; ?>
+    </td>
+  </tr>
+  <tr>
+    <td class="space">
+      <?php
+          for($i = 0; $i < count($event_id_join); $i++) {
+            //月の表示（先頭の0は表示されない）
+            //月の表示（先頭の0は表示されない）
+            if (substr($event_start_join[$i], 5, 1) == 0) {
+            echo '&nbsp;&thinsp;', substr($event_start_join[$i], 6, 1).'月';
+            } else {
+            echo substr($event_start_join[$i], 5, 2).'月';
+            }
+            //日の表示（先頭の0は表示されない）
+            if (substr($event_start_join[$i], 8, 1) == 0) {
+            echo '&ensp;&thinsp;', substr($event_start_join[$i], 9, 1).'日 ';
+            } else {
+            echo substr($event_start_join[$i], 8, 2).'日 ';
+            }
+            //イベント名とイベントの参加人数の表示
+            echo '　', '<a href=../event/event_detail.php?event_id='.$event_id_join[$i].'>'.$event_title_join[$i].'</a>',
+            '(', '現在の参加人数:', $EVCNT_JOIN[$i], '人)', '<br />';
+          }
+      ?>
+    </td>
+  </tr>
 
-<!-- 利用者が参加しているイベントの表示-->
-<p>
-<?php
-    echo $last_name[0], 'さんが参加しているイベント', '<br />';
-    for($i = 0; $i < count($event_id_join); $i++) {
-      //月の表示（先頭の0は表示されない）
-      if (substr($event_start_join[$i], 5, 1) == 0) {
-      echo substr($event_start_join[$i], 6, 1).'月';
-      } else {
-      echo substr($event_start_join[$i], 5, 2).'月';
-      }
-      //日の表示（先頭の0は表示されない）
-      if (substr($event_start_join[$i], 8, 1) == 0) {
-      echo substr($event_start_join[$i], 9, 1).'日 ';
-      } else {
-      echo substr($event_start_join[$i], 8, 2).'日 ';
-      }
-      //イベント名とイベントの参加人数の表示
-      echo '　', '<a href=http://localhost/kocote/event/event_detail.php?event_id='.$event_id_join[$i].'>'.$event_title_join[$i].'</a>',
-      '(', '現在の参加人数:', $EVCNT_JOIN[$i], '人)', '<br />';
-    }
-?>
-</p>
+  <!-- 利用者がお気に入り登録しているイベントの表示-->
+  <tr>
+    <td class="name-size">
+      <?php echo $last_name[0], 'さんがお気に入り登録しているイベント', '<br />'; ?>
+    </td>
+  </tr>
+  <tr>
+    <td class="space">
+      <?php
+          for($i = 0; $i < count($event_id_fev); $i++) {
+            //月の表示（先頭の0は表示されない）
+            if (substr($event_start_fev[$i], 5, 1) == 0) {
+            echo '&nbsp;&thinsp;', substr($event_start_fev[$i], 6, 1).'月';
+            } else {
+            echo substr($event_start_fev[$i], 5, 2).'月';
+            }
+            //日の表示（先頭の0は表示されない）
+            if (substr($event_start_fev[$i], 8, 1) == 0) {
+            echo '&ensp;&thinsp;', substr($event_start_fev[$i], 9, 1).'日 ';
+            } else {
+            echo substr($event_start_fev[$i], 8, 2).'日 ';
+            }
+            //イベント名とイベントの参加人数の表示
+            echo '　', '<a href=../event/event_detail.php?event_id='.$event_id_fev[$i].'>'.$event_title_fev[$i].'</a>',
+            '(', '現在の参加人数:', $EVCNT_FEV[$i], '人)', '<br />';
+          }
+      ?>
+    </td>
+  </tr>
+  <tr><td>　</td></tr>
+  <tr><td>　</td></tr>
 
-<!-- 利用者がお気に入り登録しているイベントの表示-->
-<p>
-<?php
-    echo $last_name[0], 'さんが参加しているイベント', '<br />';
-    for($i = 0; $i < count($event_id_fev); $i++) {
-      //月の表示（先頭の0は表示されない）
-      if (substr($event_start_fev[$i], 5, 1) == 0) {
-      echo substr($event_start_fev[$i], 6, 1).'月';
-      } else {
-      echo substr($event_start_fev[$i], 5, 2).'月';
-      }
-      //日の表示（先頭の0は表示されない）
-      if (substr($event_start_fev[$i], 8, 1) == 0) {
-      echo substr($event_start_fev[$i], 9, 1).'日 ';
-      } else {
-      echo substr($event_start_fev[$i], 8, 2).'日 ';
-      }
-      //イベント名とイベントの参加人数の表示
-      echo '　', '<a href=http://localhost/kocote/event/event_detail.php?event_id='.$event_id_fev[$i].'>'.$event_title_fev[$i].'</a>',
-      '(', '現在の参加人数:', $EVCNT_FEV[$i], '人)', '<br />';
-    }
-?>
-</p>
+</table>
 
 <!-- 本体 -->
 
