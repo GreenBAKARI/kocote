579789a45912a23c4329d807df35cc302a4e8c9f
fukudakazuma <kfukuda@fukudakazuma-no-MacBook-Pro.local>
2016-01-17 19:15:15 +0900

    ヘッダとフッダを編集

--- event/event.php
+++ event/event.php
@@ -293,6 +293,30 @@ EOT;
 }
 ?>
 
+<?php
+    //ログイン中の利用者の名前の取得
+    $link = mysql_connect('localhost', 'root', 'root');
+    if (!$link) {
+        die('接続失敗です。' .mysql_error());
+    }
+    $db_selected = mysql_select_db('greenbakari', $link);
+    if (!$db_selected) {
+        die('データベース選択失敗です。'.mysql_error());
+    }
+    mysql_set_charset('utf8');
+    $sql_login = "SELECT USER_LAST_NAME, USER_FIRST_NAME FROM UR WHERE USER_ID = $user_id";
+    $result_login = mysql_query($sql_login, $link);
+    if (!$result_login) {
+        die('クエリが失敗しました。'.mysql_error());
+    }
+    while ($row_login = mysql_fetch_array($result_login)) {
+         $last_name_login = $row_login['USER_LAST_NAME'];
+         $first_name_login = $row_login['USER_FIRST_NAME'];
+    }
+    $name_login = $last_name_login." ".$first_name_login;
+    mysql_close($link);
+?>
+
 <!-- HTML本文 -->
 <html>
 <head>
@@ -305,19 +329,29 @@ EOT;
     <link rel="stylesheet" href="../css/ev_style.css" type="text/css">
 <body topmargin="100" bottommargin="100">
 
-<div id="headerArea"></div>
+<div id="headerArea">
+    <table>
+        <tr>
+            <td><img class="login-image" src="../img/login.jpg"></td>
+            <td><p class="login-name"><?php echo $name_login;?></p></td>
+            <td><form id="logoutForm" name="logoutForm" action="../logout.php" method="POST">
+            <input id="logout-botton" type="submit" id="logout" name="formname" value="ログアウト" >
+            </form></td>
+        </tr>
+    </table>
+</div>
 
 <form id="loginForm" name="loginForm" action="" method="POST">
   <!-- <?php echo $errorMessage ?> -->
-
-  <br><br><br><br><br><br><br>  
+  
+  <br><br><br><br><br><br>
+ 
 <!-- 機能選択ボタン -->
 <div id="box">
-  <a href="../event/event.php"><img src="../img/ev_home.jpg" height="13%" width="16%"></a>
-  <a href="../bulletin/bulletin.php"><img src="../img/bb_home.jpg" height="13%" width="16%"></a>
-  <a href="../search/search.php"><img src="../img/se_home.jpg" height="13%" width="16%"></a>
-  <a href="../dm/dm.php"><img src="../img/dm_home.jpg" height="13%" width="16%"></a>
-  <a href="../mypage/mypage.php"><img src="../img/mp_home.jpg" height="13%" width="16%"></a></div>
+  <a href="../event/event.php"><img src="../img/ev_home.jpg" height="13%"></a>
+  <a href="../bulletin/bulletin.php"><img src="../img/bb_home.jpg" height="13%"></a>
+  <a href="../search/search.php"><img src="../img/se_home.jpg" height="13%"></a>
+  <a href="../mypage/mypage.php"><img src="../img/mp_home.jpg" height="13%"></a></div>
   <br>
 </center>  
 
@@ -452,11 +486,11 @@ if ($count == 0) {
 
 <div id="footerArea">
 <ul>
-<li><a href="https://www.evol-ni.com/company/">会社概要</a></li>
+<li><a href="">会社概要</a></li>
 <li><a href="../contact/contact.php">お問い合わせ</a></li>
-<li><a href="https://secure.evol-ni.com/common/policy/">個人情報保護方針</a></li>
-<li><a href="https://www.evol-ni.com/recruit/">採用情報</a></li>
-<li><a href="https://www.evol-ni.com/sitemap/">サイトマップ</a></li>
+<li><a href="">個人情報保護方針</a></li>
+<li><a href="">採用情報</a></li>
+<li><a href="">サイトマップ</a></li>
 </ul>
 </div>
 
